5a1876da0eb8813197ba30eeb746eaf5e270d573
Rudi T <116497284+yadead@users.noreply.github.com>
2025-01-31 15:28:30 +1100

    minor changes
    added automatic username to dev blog
    started work on entry lookup and entry deletion

--- DATABASE/database.sql
+++ DATABASE/database.sql
@@ -1,4 +1,3 @@
---create table user_table (username NOT NULL, password NOT NULL);
 
 DROP TABLE IF EXISTS diary_entry;
 -- SELECT * FROM user_table;
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ def sign_up():
     print(f"User '{username}' has been signed up")
 
 def sign_in():
-    username = input("Username: ")
+    global devname
+    devname = input("Username: ")
+    username = devname
     password = input("Password: ")
     if signin(username, password):
         print(f"{username} has been logged in")
@@ -20,25 +22,43 @@ def sign_in():
         print("Invalid username or password. Please try again.")
 
 def Create_entry():
-        Developer = input("Developer Name: ")
-        Project = input("Project Name: ")
-        time_format = "%H:%M %d/%m/%Y"
-        Start_Time = input("Start Time (hh:mm dd/mm/yyyy): ")
-        End_Time = input("End Time (hh:mm dd/mm/yyyy): ")
-        start_dt = datetime.strptime(Start_Time, time_format)
-        end_dt = datetime.strptime(End_Time, time_format)
-        Time_Worked = end_dt - start_dt
-        total_minutes = Time_Worked.total_seconds() / 60
-        rounded_minutes = math.ceil(total_minutes / 15) * 15
-        rounded_hours = rounded_minutes / 60
-        now = datetime.now()
-        Diary_Entry = now.strftime("%H:%M %d/%B/%Y")
-        print(f"Diary_Entry: {Diary_Entry}")
-        Time_Worked = rounded_hours
-        print(f"Time Worked: {Time_Worked} hours")
-        Repo = input("Repository Link: ")
-        Developer_Notes = input("Developer Notes: ")
-        diary_entry(Developer, Project, Start_Time, End_Time, Diary_Entry, Time_Worked, Repo, Developer_Notes)
+    Developer = devname
+    print(f"Developer: {Developer}")
+    Project = input("Project Name: ")
+    time_format = "%H:%M %d/%m/%Y"
+    Start_Time = input("Start Time (hh:mm dd/mm/yyyy): ")
+    End_Time = input("End Time (hh:mm dd/mm/yyyy): ")
+    start_dt = datetime.strptime(Start_Time, time_format)
+    end_dt = datetime.strptime(End_Time, time_format)
+    Time_Worked = end_dt - start_dt
+    total_minutes = Time_Worked.total_seconds() / 60
+    rounded_minutes = math.ceil(total_minutes / 15) * 15
+    rounded_hours = rounded_minutes / 60
+    now = datetime.now()
+    Diary_Entry = now.strftime("%H:%M %d/%B/%Y")
+    print(f"Diary_Entry: {Diary_Entry}")
+    Time_Worked = rounded_hours
+    print(f"Time Worked: {Time_Worked} hours")
+    Repo = input("Repository Link: ")
+    Developer_Notes = input("Developer Notes: ")
+    diary_entry(Developer, Project, Start_Time, End_Time, Diary_Entry, Time_Worked, Repo, Developer_Notes)
+    print("Diary entry added")
+
+def search_entry():
+    while True:
+        choice = input("search by:\n1. Create Entry\n2. Lookup Entry\n3. Sign Out\n")
+        if choice == "1":
+            Create_entry()
+        elif choice == "2":
+            search_entry()
+        elif choice == "3":
+            menu()
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+
+def delete_entry():
+    print("nothingdoneyet")
 
 
 def menu():
@@ -49,19 +69,22 @@ def menu():
         elif choice == "2":
             sign_up()
         elif choice == "3":
-            break
+            exit()
         else:
             print("Invalid choice. Please enter 1, 2, or 3.")
 
 def menu2():
     while True:
-        choice = input("Choose an option:\n1. Create Entry\n2. Lookup Entry\n3. Sign Out\n")
+        choice = input("Choose an option:\n1. Create Entry\n2. Lookup Entry\n3. Delete Entry\n4. Sign Out\n")
         if choice == "1":
             Create_entry()
         elif choice == "2":
-            Lookup_entry()
+            search_entry()
         elif choice == "3":
+            delete_entry()
+        elif choice == "4":
             menu()
+            sign_in(devname) == None
         else:
             print("Invalid choice. Please enter 1, 2, or 3.")
 
